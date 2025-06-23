f9b516ec2bcd280be61c4ec499c19f09b30d77be
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-23 16:26:30 +0200

    chore: run csharpier

--- Roleworld/Engine/VertexArray.cs
+++ Roleworld/Engine/VertexArray.cs
@@ -15,37 +15,50 @@ public class VertexArray
         _gl = gl;
         HandleVAO = gl.GenVertexArray();
         _gl.BindVertexArray(HandleVAO);
-        
+
         // VBO
         _vbo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
-        
+
         // EBO
         _ebo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _ebo);
-
     }
 
     public unsafe void DrawVertexBuffer()
     {
         float[] vertices =
         {
-            0.5f,  0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f,  0.5f, 0.0f
-        };    
-        
-        fixed (float* buf = vertices)
-            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
-        
-        uint[] indices =
-        {
-            0u, 1u, 3u,
-            1u, 2u, 3u
+            0.5f,
+            0.5f,
+            0.0f,
+            0.5f,
+            -0.5f,
+            0.0f,
+            -0.5f,
+            -0.5f,
+            0.0f,
+            -0.5f,
+            0.5f,
+            0.0f,
         };
+
+        fixed (float* buf = vertices)
+            _gl.BufferData(
+                BufferTargetARB.ArrayBuffer,
+                (nuint)(vertices.Length * sizeof(float)),
+                buf,
+                BufferUsageARB.StaticDraw
+            );
+
+        uint[] indices = { 0u, 1u, 3u, 1u, 2u, 3u };
+
+        fixed (uint* buf = indices)
+            _gl.BufferData(
+                BufferTargetARB.ElementArrayBuffer,
+                (nuint)(indices.Length * sizeof(uint)),
+                buf,
+                BufferUsageARB.StaticDraw
+            );
     }
 }
-
-
-
