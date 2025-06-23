0f242dd939f3d67208b2de184098ba13b6bf9f9c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-23 16:33:10 +0200

    chore: adding shader vertex code

--- Roleworld/Engine/Shader.cs
+++ Roleworld/Engine/Shader.cs
@@ -2,5 +2,14 @@ namespace Roleworld.Engine;
 
 public class Shader
 {
-    
+    const string vertexCode =
+        @"
+            #version 330 core
+
+            layout (location = 0) in vec3 aPosition;
+
+            void main()
+            {
+                gl_Position = vec4(aPosition, 1.0);
+            }";
 }
