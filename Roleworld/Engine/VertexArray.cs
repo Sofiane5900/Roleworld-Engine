85cdd963f0e5fb73a59d183c42720dcb0a97700b
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-23 15:44:08 +0200

    feat: adding vbo

--- Roleworld/Engine/VertexArray.cs
+++ Roleworld/Engine/VertexArray.cs
@@ -6,6 +6,7 @@ public class VertexArray
 {
     private readonly GL _gl;
     private readonly uint _vao;
+    private readonly uint _vbo;
     public uint HandleVAO { get; }
 
     public VertexArray(GL gl)
@@ -13,6 +14,22 @@ public class VertexArray
         _gl = gl;
         HandleVAO = gl.GenVertexArray();
         _gl.BindVertexArray(HandleVAO);
+        _vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+    }
+
+    public unsafe void DrawVertexBuffer()
+    {
+        float[] vertices =
+        {
+            0.5f,  0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f
+        };    
+        
+        fixed (float* buf = vertices)
+            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
     }
 }
 
