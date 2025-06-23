ed2e5492bd503345cd48a5d1959eb3bf3b823a02
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-23 13:53:57 +0200

    feat: adding handle instead of vao

--- Roleworld/Engine/VertexArray.cs
+++ Roleworld/Engine/VertexArray.cs
@@ -6,7 +6,15 @@ public class VertexArray
 {
     private readonly GL _gl;
     private readonly uint _vao;
-    
+    public uint Handle { get; }
+
+    public VertexArray(GL gl)
+    {
+        _gl = gl;
+        Handle = gl.GenVertexArray();
+        _gl.BindVertexArray(Handle);
+    }
 }
+
 
 
