d0e9601067cde0a31431305a3761d18e2babdba8
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-23 16:23:55 +0200

    feat: element buffer object in ctor

--- Roleworld/Engine/VertexArray.cs
+++ Roleworld/Engine/VertexArray.cs
@@ -7,6 +7,7 @@ public class VertexArray
     private readonly GL _gl;
     private readonly uint _vao;
     private readonly uint _vbo;
+    private readonly uint _ebo;
     public uint HandleVAO { get; }
 
     public VertexArray(GL gl)
@@ -14,8 +15,15 @@ public class VertexArray
         _gl = gl;
         HandleVAO = gl.GenVertexArray();
         _gl.BindVertexArray(HandleVAO);
+        
+        // VBO
         _vbo = _gl.GenBuffer();
         _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+        
+        // EBO
+        _ebo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _ebo);
+
     }
 
     public unsafe void DrawVertexBuffer()
@@ -30,6 +38,12 @@ public class VertexArray
         
         fixed (float* buf = vertices)
             _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+        
+        uint[] indices =
+        {
+            0u, 1u, 3u,
+            1u, 2u, 3u
+        };
     }
 }
 
