5d6b12964189970f22c537257a28492828747da6
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-25 15:32:21 +0200

    feat: add gl shader vao and vertex count properities in renderable object

--- Roleworld/Engine/Rendering/RenderableObject.cs
+++ Roleworld/Engine/Rendering/RenderableObject.cs
@@ -1,3 +1,11 @@
+using Silk.NET.OpenGL;
+
 namespace Roleworld.Engine;
 
-public class RenderableObject { }
+public class RenderableObject
+{
+    private readonly GL _gl;
+    private readonly Shader _shader;
+    private readonly uint _vao;
+    private readonly uint _vertexCount;
+}
