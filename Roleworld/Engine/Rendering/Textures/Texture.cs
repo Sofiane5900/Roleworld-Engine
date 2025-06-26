074088bff14d0cb443cdeeae93bc319b7b348e6f
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-26 11:29:54 +0200

    feat: adding height width and id properties to texture class

--- Roleworld/Engine/Rendering/Textures/Texture.cs
+++ Roleworld/Engine/Rendering/Textures/Texture.cs
@@ -1,3 +1,8 @@
 namespace Roleworld.Engine.Textures;
 
-public class Texture { }
+public class Texture
+{
+    public uint Id { get; }
+    public int Width { get; }
+    public int Height { get; }
+}
