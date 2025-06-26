810aa8e55d84e3863aba511f975cb86212eaed8c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-26 15:19:58 +0200

    feat: configure texture wrapping in ctor

--- Roleworld/Engine/Rendering/Textures/Texture.cs
+++ Roleworld/Engine/Rendering/Textures/Texture.cs
@@ -15,5 +15,27 @@ public class Texture
         var image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
 
         Id = gl.GenTexture();
+        gl.BindTexture(TextureTarget.Texture2D, Id);
+
+        gl.TexParameter(
+            TextureTarget.Texture2D,
+            TextureParameterName.TextureWrapS,
+            (int)GLEnum.Repeat
+        );
+        gl.TexParameter(
+            TextureTarget.Texture2D,
+            TextureParameterName.TextureWrapT,
+            (int)GLEnum.Repeat
+        );
+        gl.TexParameter(
+            TextureTarget.Texture2D,
+            TextureParameterName.TextureMinFilter,
+            (int)GLEnum.Linear
+        );
+        gl.TexParameter(
+            TextureTarget.Texture2D,
+            TextureParameterName.TextureMagFilter,
+            (int)GLEnum.Linear
+        );
     }
 }
