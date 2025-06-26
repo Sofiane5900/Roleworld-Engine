0cbafc59c13bbf82337d3a48acc360cec36d1114
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-26 11:31:18 +0200

    feat: add texture ctor

--- Roleworld/Engine/Rendering/Textures/Texture.cs
+++ Roleworld/Engine/Rendering/Textures/Texture.cs
@@ -1,3 +1,5 @@
+using Silk.NET.OpenGL;
+
 namespace Roleworld.Engine.Textures;
 
 public class Texture
@@ -5,4 +7,6 @@ public class Texture
     public uint Id { get; }
     public int Width { get; }
     public int Height { get; }
+
+    public Texture(GL gl, string path) { }
 }
