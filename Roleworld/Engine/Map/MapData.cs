60aace63c856dbd233c0c94b05977b9679914882
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-27 13:27:11 +0200

    feat: add map data properties

--- Roleworld/Engine/Map/MapData.cs
+++ Roleworld/Engine/Map/MapData.cs
@@ -1,3 +1,9 @@
 namespace Roleworld.Engine.Map;
 
-public class MapData { }
+public class MapData
+{
+    public int Width { get; }
+    public int Height { get; }
+    public float[,] HeightMap { get; }
+    public Biome[,] BiomeMap { get; }
+}
