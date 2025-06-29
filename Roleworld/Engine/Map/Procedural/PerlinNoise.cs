8bcd6a0f8901767422ec823af47ca339426d5ca1
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-29 17:41:56 +0200

    feat: determine grid cell cordinates generate perlin method

--- Roleworld/Engine/Map/Procedural/PerlinNoise.cs
+++ Roleworld/Engine/Map/Procedural/PerlinNoise.cs
@@ -1,6 +1,15 @@
+using System.Xml;
+
 namespace Roleworld.Engine.Map;
 
 public class PerlinNoise
 {
-    
+    public float GeneratePerlin(float x, float y)
+    {
+        // determine grid cell cordinates
+        int x0 = (int)x;
+        int y0 = (int)y;
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+    }
 }
