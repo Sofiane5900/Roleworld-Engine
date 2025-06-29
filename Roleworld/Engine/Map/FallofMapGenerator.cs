8ceb695c24f83d91eeaa80d2c8cce46da6339701
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-29 16:03:31 +0200

    feat: evaluate fonction for the fallofmap generator

--- Roleworld/Engine/Map/FallofMapGenerator.cs
+++ Roleworld/Engine/Map/FallofMapGenerator.cs
@@ -1,3 +1,13 @@
 namespace Roleworld.Engine.Map;
 
-public class FallofMapGenerator { }
+public static class FallofMapGenerator
+{
+    private static float Evaluate(float value)
+    {
+        float a = 3f;
+        float b = 2.2f;
+
+        // return a smooth transition from the center of the map to the edge
+        return MathF.Pow(value, a) / (MathF.Pow(value, a) + MathF.Pow(b - b * value, a));
+    }
+}
