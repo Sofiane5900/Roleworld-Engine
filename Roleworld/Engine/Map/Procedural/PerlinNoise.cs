b5dfba6caddae718b77e9ef575129bbaeb1fbda7
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-06-29 17:53:58 +0200

    feat: add generateperlin method in perlin class

--- Roleworld/Engine/Map/Procedural/PerlinNoise.cs
+++ Roleworld/Engine/Map/Procedural/PerlinNoise.cs
@@ -26,4 +26,33 @@ public class PerlinNoise
     {
         return a + t * (b - a);
     }
+
+    public float GeneratePerlin(float x, float y)
+    {
+        int x0 = (int)MathF.Floor(x);
+        int x1 = x0 + 1;
+        int y0 = (int)MathF.Floor(y);
+        int y1 = y0 + 1;
+
+        float sx = Fade(x - x0);
+        float sy = Fade(y - y0);
+
+        float n0,
+            n1,
+            ix0,
+            ix1,
+            value;
+
+        n0 = DotGridGradient(x0, y0, x, y);
+        n1 = DotGridGradient(x1, y0, x, y);
+        ix0 = Lerp(n0, n1, sx);
+
+        n0 = DotGridGradient(x0, y1, x, y);
+        n1 = DotGridGradient(x1, y1, x, y);
+        ix1 = Lerp(n0, n1, sx);
+
+        value = Lerp(ix0, ix1, sy);
+
+        return value;
+    }
 }
