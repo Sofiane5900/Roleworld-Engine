96fd6af270bf6835b86fa2560090df743c84ef99
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-02 16:01:44 +0200

    feat: add camera properties

--- Roleworld/Engine/Rendering/Camera2D.cs
+++ Roleworld/Engine/Rendering/Camera2D.cs
@@ -2,5 +2,10 @@ namespace Roleworld.Engine;
 
 public class Camera2D
 {
-    
+    public float X { get; set; } = 0f;
+    public float Y { get; set; } = 0f;
+    public float Zoom { get; set; } = 1f;
+
+    public float MinZoom { get; set; } = 0.1f;
+    public float MaxZoom { get; set; } = 10f;
 }
