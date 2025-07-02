7abc0eb568fa1ec7f09934db0dcd1b27ea23fcbf
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-02 22:14:05 +0200

    refactor: delete vector2 struct in perlin noise

--- Roleworld/Engine/Map/Procedural/PerlinNoise.cs
+++ Roleworld/Engine/Map/Procedural/PerlinNoise.cs
@@ -35,19 +35,7 @@ public class PerlinNoise
     }
 
     // 2D Gradient Vector
-    private struct Vector2
-    {
-        public float X,
-            Y;
 
-        public Vector2(float x, float y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public float Dot(Vector2 other) => X * other.X + Y * other.Y;
-    }
 
     private static readonly Vector2[] Gradients = new[]
     {
