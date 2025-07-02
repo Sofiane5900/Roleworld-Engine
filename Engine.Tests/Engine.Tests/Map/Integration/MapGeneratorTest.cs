6db0fdce9042aa28c58b4853b66b6019002f68f6
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-02 10:46:26 +0200

    test: add integration test map generator

--- Engine.Tests/Engine.Tests/Map/Integration/MapGeneratorTest.cs
+++ Engine.Tests/Engine.Tests/Map/Integration/MapGeneratorTest.cs
@@ -4,7 +4,6 @@ namespace Engine.Tests.Map.Integration;
 
 public class MapGeneratorTest
 {
-
     [Fact]
     public void GenerateMap_WithSeed_ReturnsConsistentAndValidMap()
     {
@@ -16,8 +15,15 @@ public class MapGeneratorTest
         Assert.NotNull(map);
         Assert.Equal(256, map.Width);
         Assert.Equal(256, map.Height);
-        
+
         var map2 = mapGen.Generate(256, 256);
-        Assert.Equal(map, map2);
+        for (int x = 0; x < map.Width; x++)
+        {
+            for (int y = 0; y < map.Height; y++)
+            {
+                Assert.Equal(map.HeightMap[x, y], map2.HeightMap[x, y]);
+                Assert.Equal(map.BiomeMap[x, y], map2.BiomeMap[x, y]);
+            }
+        }
     }
 }
