50af0effb531e77f3c1ee84ac24a8547f1bd0fe9
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-02 22:30:56 +0200

    feat: add more zoom into the camera 2D

--- Roleworld/Engine/Rendering/Camera2D.cs
+++ Roleworld/Engine/Rendering/Camera2D.cs
@@ -6,7 +6,7 @@ public class Camera2D
 {
     public float X { get; set; } = 0f;
     public float Y { get; set; } = 0f;
-    public float Zoom { get; set; } = 1f;
+    public float Zoom { get; set; } = 1.6f;
 
     public float MinZoom { get; set; } = 0.1f;
     public float MaxZoom { get; set; } = 10f;
