3e0bad9c49cca1787a7af95adf8b4a0fb5d7a56f
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-04 00:00:02 +0200

    feat: add voronoi class logic

--- Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
@@ -17,5 +17,43 @@ public class Voronoi
         Sites.Add(point);
     }
 
-    public void Generate(RectangleF bounds) { }
+    public void Generate(RectangleF bounds)
+    {
+        if (Sites.Count == 0)
+        {
+            return;
+        }
+
+        Edges = VoronoiPlane.TessellateOnce(
+            Sites,
+            bounds.Left,
+            bounds.Top,
+            bounds.Right,
+            bounds.Bottom
+        );
+
+        // remove old cells
+        Cells.Clear();
+
+        foreach (var site in Sites)
+        {
+            var cell = new VoronoiCell(new VoronoiSite(site.X, site.Y));
+
+            // get all the vertices of the cell
+            var edgesForSite = Edges.Where(e => e.Left == site || e.Right == site).ToList();
+
+            var points = new List<Vector2>();
+
+            foreach (var edge in edgesForSite)
+            {
+                if (edge.Start != null)
+                    points.Add(new Vector2((float)edge.Start.X, (float)edge.Start.Y));
+                if (edge.End != null)
+                    points.Add(new Vector2((float)edge.End.X, (float)edge.End.Y));
+            }
+
+            //  avoid duplicates, keep only dinstinct point
+            cell.Vertices.AddRange(points.Distinct());
+        }
+    }
 }
