4abea2f8fb74c11329b7bf4f08925e5e0ae92323
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-03 15:54:03 +0200

    feat: add addSite method to vornoi class

--- Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
@@ -7,4 +7,9 @@ public class Voronoi
     public List<Vector2> Sites { get; private set; } = new();
 
     public Voronoi() { }
+
+    public void AddSite(Vector2 point)
+    {
+        Sites.Add(point);
+    }
 }
