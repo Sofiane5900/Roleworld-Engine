809ad08b51d76dd9ebf0243fdadb0b2dc85b73b3
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-03 13:46:07 +0200

    feat: add onupdate method to camera controller

--- Roleworld/Engine/Rendering/Camera/CameraController.cs
+++ Roleworld/Engine/Rendering/Camera/CameraController.cs
@@ -6,4 +6,18 @@ public class CameraController
 {
     private static Camera2D _camera;
     private static IKeyboard keyboard;
+
+    private static void OnUpdate(double deltaTime)
+    {
+        float cameraSpeed = 300f * (float)deltaTime;
+
+        if (keyboard.IsKeyPressed(Key.W) || keyboard.IsKeyPressed(Key.Down))
+            _camera.Pan(0, -cameraSpeed);
+        if (keyboard.IsKeyPressed(Key.S) || keyboard.IsKeyPressed(Key.Up))
+            _camera.Pan(0, cameraSpeed);
+        if (keyboard.IsKeyPressed(Key.A) || keyboard.IsKeyPressed(Key.Left))
+            _camera.Pan(-cameraSpeed, 0);
+        if (keyboard.IsKeyPressed(Key.D) || keyboard.IsKeyPressed(Key.Right))
+            _camera.Pan(cameraSpeed, 0);
+    }
 }
