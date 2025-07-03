af868cf83fab1e2c1426133d31ca0161dc2dd905
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-03 15:55:14 +0200

    feat: add site & vertices properties in voronoi cell clas

--- Roleworld/Engine/Map/Procedural/Voronoi/VoronoiCell.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/VoronoiCell.cs
@@ -1,6 +1,9 @@
+using System.Numerics;
+
 namespace Roleworld.Engine.Map.Voronoi;
 
 public class VoronoiCell
 {
-    
+    public Vector2 Site { get; }
+    public List<Vector2> Vertices { get; } = new();
 }
