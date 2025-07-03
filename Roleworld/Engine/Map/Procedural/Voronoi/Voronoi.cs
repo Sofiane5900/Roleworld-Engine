d8638c631a7ef7f5cbd7a9658a4266207b3ca33c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-03 15:53:35 +0200

    feat: add voronoi sites property

--- Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Voronoi.cs
@@ -2,4 +2,7 @@ using System.Numerics;
 
 namespace Roleworld.Engine.Map.Voronoi;
 
-public class Voronoi { }
+public class Voronoi
+{
+    public List<Vector2> Sites { get; private set; } = new();
+}
