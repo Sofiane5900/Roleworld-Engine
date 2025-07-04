62f64c41da9a9f2f10f6f789383b3c594376d4ab
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-04 14:05:35 +0200

    feat: add terrain type property to voronoi cell

--- Roleworld/Engine/Map/Procedural/Voronoi/VoronoiCell.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/VoronoiCell.cs
@@ -8,6 +8,8 @@ public class VoronoiCell
     public VoronoiSite Site { get; }
     public List<Vector2> Vertices { get; } = new();
 
+    public TerrainType TerrainType { get; set; }
+
     public VoronoiCell(VoronoiSite site)
     {
         Site = site;
