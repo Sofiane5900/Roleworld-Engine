7d1ae7db0884aa947e47652ef6c4548c35873276
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-04 23:34:54 +0200

    feat: add get edge unique id method to noisy edge

--- Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
@@ -10,4 +10,16 @@ public class NoisyEdge
     public Vector2 SiteB;
 
     public List<Vector2> NoisyPoints = new();
+
+    public static string GetEdgeId(Vector2 a, Vector2 b)
+    {
+        if (a.X < b.X || (a.X == b.X && a.Y < b.Y))
+        {
+            return $"{a.X:F2},{a.Y:F2}-{b.X:F2},{b.Y:F2}";
+        }
+        else
+        {
+            return $"{b.X:F2},{b.Y:F2}-{a.X:F2},{a.Y:F2}";
+        }
+    }
 }
