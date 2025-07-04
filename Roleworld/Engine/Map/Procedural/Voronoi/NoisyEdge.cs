e3f5eaab0ad70666feb96dd4c3cc695089ee3566
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-04 23:31:47 +0200

    feat: add noisy edge properties

--- Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
@@ -1,6 +1,13 @@
-namespace Roleworld.Engine.Map.Voronoi;
+using System.Numerics;
+
+namespace Roleworld.Engine.Map.Procedural.Voronoi;
 
 public class NoisyEdge
 {
-    
+    public Vector2 Corner1;
+    public Vector2 Corner2;
+    public Vector2 SiteA;
+    public Vector2 SiteB;
+
+    public List<Vector2> NoisyPoints = new();
 }
