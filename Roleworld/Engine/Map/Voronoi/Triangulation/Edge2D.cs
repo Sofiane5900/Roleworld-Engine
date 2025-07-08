d39c53b136e74f9c9388b4fe8f5f95c888a19116
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-08 14:29:37 +0200

    feat: add struct edge2d properties

--- Roleworld/Engine/Map/Voronoi/Triangulation/Edge2D.cs
+++ Roleworld/Engine/Map/Voronoi/Triangulation/Edge2D.cs
@@ -2,4 +2,8 @@ using System.Numerics;
 
 namespace Roleworld.Engine.Map.Voronoi;
 
-public readonly struct Edge2D { }
+public readonly struct Edge2D
+{
+    public Vector2 A { get; }
+    public Vector2 B { get; }
+}
