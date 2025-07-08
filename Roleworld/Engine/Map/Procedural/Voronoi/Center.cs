2f0c13dcb2eb8ab7a2e53de2b80b465746b91395
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-08 12:15:14 +0200

    feat: add Center main properties

--- Roleworld/Engine/Map/Procedural/Voronoi/Center.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Center.cs
@@ -1,6 +1,10 @@
+using System.Numerics;
+
 namespace Roleworld.Engine.Map.Voronoi;
 
 public class Center
 {
-    
+    public Vector2 Position;
+    public bool IsLand;
+    public float Elevation;
 }
