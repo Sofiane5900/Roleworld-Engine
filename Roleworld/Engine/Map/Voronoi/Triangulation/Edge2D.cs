9c6281b45ccb1ebdb3e317e3ea2b07ccea67d821
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-08 14:30:04 +0200

    feat: add edge2d ctor

--- Roleworld/Engine/Map/Voronoi/Triangulation/Edge2D.cs
+++ Roleworld/Engine/Map/Voronoi/Triangulation/Edge2D.cs
@@ -6,4 +6,10 @@ public readonly struct Edge2D
 {
     public Vector2 A { get; }
     public Vector2 B { get; }
+
+    public Edge2D(Vector2 a, Vector2 b)
+    {
+        A = a;
+        B = b;
+    }
 }
