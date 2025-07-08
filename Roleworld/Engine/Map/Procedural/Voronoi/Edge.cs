df1155f8ef43ae897df4d12617a7cca4faa40fae
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-08 12:23:16 +0200

    feat: add midpoint property to edge class

--- Roleworld/Engine/Map/Procedural/Voronoi/Edge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Edge.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Roleworld.Engine.Map.Voronoi;
 
 public class Edge
@@ -6,6 +8,7 @@ public class Edge
     public Corner Corner1;
     public Center Center0;
     public Center Center1;
+    public Vector2 Midpoint => (Corner0.Position + Corner1.Position) / 2f;
 
     public Edge() { }
 }
