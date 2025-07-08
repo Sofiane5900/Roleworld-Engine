6c4413befe493f4ad15e4efc792295cc6b2af025
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-08 12:22:18 +0200

    feat: add edge class properties corners &centers

--- Roleworld/Engine/Map/Procedural/Voronoi/Edge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/Edge.cs
@@ -1,3 +1,9 @@
 namespace Roleworld.Engine.Map.Voronoi;
 
-public class Edge { }
+public class Edge
+{
+    public Corner Corner0;
+    public Corner Corner1;
+    public Center Center0;
+    public Center Center1;
+}
