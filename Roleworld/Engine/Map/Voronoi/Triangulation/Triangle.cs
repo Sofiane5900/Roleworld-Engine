82544704e75881f442d17d38936241905a9e44c6
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 10:51:15 +0200

    feat: add ContainsInCircumcircle method in triangle class

--- Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
+++ Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
@@ -17,4 +17,21 @@ public class Triangle
 
     public List<Edge2D> GetEdges() =>
         new() { new Edge2D(A, B), new Edge2D(B, C), new Edge2D(C, A) };
+
+    public bool ContainsInCircumcircle(Vector2 p)
+    {
+        float ax = A.X - p.X;
+        float ay = A.Y - p.Y;
+        float bx = B.X - p.X;
+        float by = B.Y - p.Y;
+        float cx = C.X - p.X;
+        float cy = C.Y - p.Y;
+
+        float det =
+            (ax * ax + ay * ay) * (bx * cy - by * cx)
+            - (bx * bx + by * by) * (ax * cy - ay * cx)
+            + (cx * cx + cy * cy) * (ax * by - ay * bx);
+
+        return det > 0;
+    }
 }
