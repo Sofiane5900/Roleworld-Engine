afaddfd0b133d074f091bdaa2ac0df20951b934c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 14:17:13 +0200

    feat: add cell ctor

--- Roleworld/Engine/Map/Voronoi/Cell.cs
+++ Roleworld/Engine/Map/Voronoi/Cell.cs
@@ -7,4 +7,10 @@ public class Cell
     public Center Center { get; }
     public List<Vector2> Vertices { get; }
     public TerrainType TerrainType { get; set; }
+
+    public Cell(Center center, List<Vector2> vertices)
+    {
+        Center = center;
+        Vertices = vertices;
+    }
 }
