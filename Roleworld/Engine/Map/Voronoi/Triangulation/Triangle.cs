955cbb36f680e96f45c58ca869efb40b1e8bddbd
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 10:37:48 +0200

    feat: add triangle ctor

--- Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
+++ Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
@@ -7,4 +7,11 @@ public class Triangle
     public Vector2 A { get; }
     public Vector2 B { get; }
     public Vector2 C { get; }
+
+    public Triangle(Vector2 a, Vector2 b, Vector2 c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
 }
