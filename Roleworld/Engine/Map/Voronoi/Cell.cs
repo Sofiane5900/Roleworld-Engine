3d4a21553e74a0bd6c8066372ca6a9df466e820f
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 14:17:08 +0200

    feat: add cell voronoi class properties

--- Roleworld/Engine/Map/Voronoi/Cell.cs
+++ Roleworld/Engine/Map/Voronoi/Cell.cs
@@ -2,4 +2,9 @@ using System.Numerics;
 
 namespace Roleworld.Engine.Map.Voronoi;
 
-public class Cell { }
+public class Cell
+{
+    public Center Center { get; }
+    public List<Vector2> Vertices { get; }
+    public TerrainType TerrainType { get; set; }
+}
