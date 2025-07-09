e172783979fc62c6a70ba658dc5fdc97f163d93f
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 10:19:04 +0200

    feat: add triangle properties

--- Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
+++ Roleworld/Engine/Map/Voronoi/Triangulation/Triangle.cs
@@ -2,4 +2,9 @@ using System.Numerics;
 
 namespace Roleworld.Engine.Map.Voronoi;
 
-public class Triangle { }
+public class Triangle
+{
+    public Vector2 A { get; }
+    public Vector2 B { get; }
+    public Vector2 C { get; }
+}
