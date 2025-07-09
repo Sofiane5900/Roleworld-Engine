5a2e5a6b2615dc522efb62935f592825ecf51e43
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-09 12:13:51 +0200

    feat: add edge class ctor

--- Roleworld/Engine/Map/Voronoi/Edge.cs
+++ Roleworld/Engine/Map/Voronoi/Edge.cs
@@ -10,5 +10,11 @@ public class Edge
     public Center Center1;
     public Vector2 Midpoint => (Corner0.Position + Corner1.Position) / 2f;
 
-    public Edge() { }
+    public Edge(Corner corner0, Corner corner1, Center center0, Center center1)
+    {
+        Corner0 = corner0;
+        Corner1 = corner1;
+        Center0 = center0;
+        Center1 = center1;
+    }
 }
