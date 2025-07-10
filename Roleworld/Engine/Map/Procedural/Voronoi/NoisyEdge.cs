d157fc60c1963d0b46832fd708c92b6f4cf544bf
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-10 14:37:11 +0200

    feat: add noisyedge properties

--- Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
@@ -1,6 +1,10 @@
+using System.Numerics;
+using SharpVoronoiLib;
+
 namespace Roleworld.Engine.Map.Voronoi;
 
 public class NoisyEdge
 {
-    
+    public VoronoiEdge Edge;
+    public List<Vector2> NoisyPoints;
 }
