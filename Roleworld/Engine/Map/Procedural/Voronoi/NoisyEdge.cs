2932574efa07e7265151ad291c74e34172297f9c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-10 14:39:11 +0200

    chore: add xml comments to noisy edge class

--- Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
+++ Roleworld/Engine/Map/Procedural/Voronoi/NoisyEdge.cs
@@ -3,6 +3,9 @@ using SharpVoronoiLib;
 
 namespace Roleworld.Engine.Map.Voronoi;
 
+/// <summary>
+///  Data-strcture which aims to contain Voronoi Edges with its noisy render points.
+/// </summary>
 public class NoisyEdge
 {
     public VoronoiEdge Edge;
