16a44e18fc416cd642b8687686282fe0fa4edb0c
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-10 09:40:54 +0200

    feat: reduce map number of voronoi cells to 1000

--- Roleworld/Engine/Map/MapGenerator.cs
+++ Roleworld/Engine/Map/MapGenerator.cs
@@ -42,7 +42,7 @@ namespace Roleworld.Engine.Map
             // 2. Voronoi generation
             voronoi = new Voronoi.Voronoi(0, 0, width, height);
 
-            int nbSites = 4000;
+            int nbSites = 1000;
             var rand = new Random();
 
             for (int i = 0; i < nbSites; i++)
