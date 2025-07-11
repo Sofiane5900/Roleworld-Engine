1067e4e78befc4cb573e196f116cf04a78ef5c7a
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-11 09:52:21 +0200

    feat: add generate method of HeightMapGenerator

--- Roleworld/Engine/Map/Generator/HeightMapGenerator.cs
+++ Roleworld/Engine/Map/Generator/HeightMapGenerator.cs
@@ -1,6 +1,30 @@
-namespace Roleworld.Engine.Map.Generator;
+using Roleworld.Engine.Map.Procedural.Noise;
 
-public class HeightMapGenerator
+namespace Roleworld.Engine.Map.Generators;
+
+public static class HeightMapGenerator
 {
-    
+    /// <summary>
+    /// Generate a normalized [0,1] matrix combining
+    /// Perlin noise and a Fallof ma.
+    /// </summary>
+    /// <param name="width">width of the map</param>
+    /// <param name="height">height of the map</param>
+    /// <param name="seed">seed of the map</param>
+    /// <returns>A float matrix with datas of elevation</returns>
+    public static float[,] Generate(int width, int height, int seed = 0)
+    {
+        var perlin = new PerlinNoise(seed);
+        var falloff = Procedural.Noise.FallofMapGenerator.Generate(width, height);
+        float[,] map = new float[width, height];
+        float scale = 150f;
+
+        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
+        {
+            float noise = perlin.GenerateNormalizedNoise(x / scale, y / scale);
+            map[x, y] = Math.Clamp(noise - falloff[x, y], 0f, 1f);
+        }
+        return map;
+    }
 }
