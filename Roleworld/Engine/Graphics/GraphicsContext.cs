c5661e281ba4c3fdeb4deb4de52467d923347755
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-15 11:25:32 +0200

    feat: add Initialize method in GraphicsContext class

--- Roleworld/Engine/Graphics/GraphicsContext.cs
+++ Roleworld/Engine/Graphics/GraphicsContext.cs
@@ -1,9 +1,21 @@
+using System.Drawing;
 using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
 
 namespace Roleworld.Engine.Graphics;
 
 public class GraphicsContext
 {
-    private static GL _gl = null!;
+    public static GL _gl = null!;
     private static Shader _shader = null!;
+    private static IWindow _mainWindow = null!;
+    public GL Gl => _gl;
+    public Shader Shader => _shader;
+
+    public void Initialize(IWindow window)
+    {
+        _gl = window.CreateOpenGL();
+        _shader = new Shader(_gl);
+        _gl.ClearColor(Color.Blue);
+    }
 }
