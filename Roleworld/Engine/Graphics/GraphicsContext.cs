b437d103a79f4edbef4e771405828bbf89d6f8d1
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-15 10:38:31 +0200

    feat: add GraphicsContext properties

--- Roleworld/Engine/Graphics/GraphicsContext.cs
+++ Roleworld/Engine/Graphics/GraphicsContext.cs
@@ -1,6 +1,9 @@
+using Silk.NET.OpenGL;
+
 namespace Roleworld.Engine.Graphics;
 
 public class GraphicsContext
 {
-    
+    private static GL _gl = null!;
+    private static Shader _shader = null!;
 }
