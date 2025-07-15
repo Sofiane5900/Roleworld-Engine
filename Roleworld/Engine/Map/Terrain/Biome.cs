2799fe7df67fcd661518a858a34d495b9526b86e
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-15 15:54:27 +0200

    feat: add biome enum class

---
+++ Roleworld/Engine/Map/Terrain/Biome.cs
@@ -0,0 +1,18 @@
+namespace Roleworld.Engine.Map;
+
+public enum Biome
+{
+    Snow,
+    Tundra,
+    Bare,
+    Scorched,
+    Taiga,
+    Shrubland,
+    TemperateDesert,
+    TemperateRainForest,
+    TemperateDeciduousForest,
+    Grassland,
+    TropicalRainForest,
+    TropicalSeasonalForest,
+    SubtropicalDesert,
+}
