4f66921ebd1815a1a76b18edfed18126d71cd512
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-15 12:15:29 +0200

    feat: add WorldManager properties

--- Roleworld/Engine/Map/WorldManager.cs
+++ Roleworld/Engine/Map/WorldManager.cs
@@ -1,6 +1,9 @@
+using Roleworld.Engine.Rendering;
+
 namespace Roleworld.Engine.Map;
 
 public class WorldManager
 {
-    
+    private static MapRenderer _mapRenderer = null!;
+    private static MapData _mapData = null!;
 }
