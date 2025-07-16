d3a5bad4d0a3317d3e6918f778856b425c788541
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-07-16 11:42:27 +0200

    feat: add generate method in moisture map generator class

--- Roleworld/Engine/Map/Procedural/Noise/MoistureMapGenerator.cs
+++ Roleworld/Engine/Map/Procedural/Noise/MoistureMapGenerator.cs
@@ -1,3 +1,20 @@
 namespace Roleworld.Engine.Map.Procedural.Noise;
 
-public class MoistureMapGenerator { }
+public class MoistureMapGenerator
+{
+    public float[,] Generate(int width, int height, int seed)
+    {
+        var perlin = new PerlinNoise(seed * 7919);
+        float[,] map = new float[width, height];
+        float scale = 500f;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[x, y] = perlin.GenerateNormalizedNoise(x / scale, y / scale);
+            }
+        }
+        return map;
+    }
+}
